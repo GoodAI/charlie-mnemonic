22ee646719c68a3293e5ffa0f5e475b58500ba78
alloin <webmaster@airobin.net>
2023-08-14 03:50:53 +0200

    Update authentication.py
    to postreg

--- authentication.py
+++ authentication.py
@@ -45,7 +45,7 @@ class Authentication:
             # Convert hashed_password to string
             hashed_password_str = hashed_password.decode("utf-8")
             session_token = binascii.hexlify(os.urandom(24)).decode()
-            self.cursor.execute("INSERT INTO users (username, password, session_token) VALUES (?, ?, ?)", (username, hashed_password_str, session_token))
+            self.cursor.execute("INSERT INTO users (username, password, session_token) VALUES (%s, %s, %s)", (username, hashed_password_str, session_token))
             self.conn.commit()
             return session_token
         except psycopg2.IntegrityError:
@@ -55,7 +55,7 @@ class Authentication:
 
     def login(self, username, password):
         self.open()
-        self.cursor.execute("SELECT password FROM users WHERE username = ?", (username,))
+        self.cursor.execute("SELECT password FROM users WHERE username = %s", (username,))
         result = self.cursor.fetchone()
 
         if result is not None:
@@ -68,7 +68,7 @@ class Authentication:
                 # If password is correct, generate a session token
                 session_token = binascii.hexlify(os.urandom(24)).decode()
                 # Store the session token in the database
-                self.cursor.execute("UPDATE users SET session_token = ? WHERE username = ?", (session_token, username))
+                self.cursor.execute("UPDATE users SET session_token = %s WHERE username = %s", (session_token, username))
                 self.conn.commit()
                 self.close()
                 print(f'User: {username} - Session token: {session_token}')
@@ -81,14 +81,14 @@ class Authentication:
         
     def logout(self, username, session_token):
         self.open()
-        self.cursor.execute("SELECT session_token FROM users WHERE username = ?", (username,))
+        self.cursor.execute("SELECT session_token FROM users WHERE username = %s", (username,))
         result = self.cursor.fetchone()
 
         if result is not None:
             stored_token = result[0]
             if stored_token == session_token:
                 # If the session token is correct, delete it from the database
-                self.cursor.execute("UPDATE users SET session_token = NULL WHERE username = ?", (username,))
+                self.cursor.execute("UPDATE users SET session_token = NULL WHERE username = %s", (username,))
                 self.conn.commit()
                 self.close()
                 return True
@@ -99,7 +99,7 @@ class Authentication:
 
     def check_token(self, username, session_token):
         self.open()
-        self.cursor.execute("SELECT session_token FROM users WHERE username = ?", (username,))
+        self.cursor.execute("SELECT session_token FROM users WHERE username = %s", (username,))
         result = self.cursor.fetchone()
 
         if result is not None:

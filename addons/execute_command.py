7b0ac7dffff5b36eb03f016e4b12181aedd2c176
alloin <webmaster@airobin.net>
2023-08-14 13:24:26 +0200

    changing addons for webversion
    most ran local so no use in adding these here

---
+++ addons/execute_command.py
@@ -0,0 +1,28 @@
+import json
+
+description = "Execute a command and return a success message"
+parameters = {
+    "type": "object",
+    "properties": {
+        "command": {
+            "type": "string",
+            "description": "The command to execute",
+        }
+    },
+    "required": ["command"],
+}
+
+def execute_command(command):
+    # Parse the command
+    command = json.loads(command)
+
+    # Execute the command
+    # In a real-world scenario, the command execution logic goes here.
+    # For this example, we'll just print it.
+    print(f"Executing command: {command}")
+
+    # Return a success message
+    return "Command executed successfully"
+
+# Test the function
+print(execute_command(json.dumps({"command": "Hello, World!"})))

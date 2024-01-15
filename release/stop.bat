46f675fc63d22a2776b197de17cba719158fe713
Karel Hovorka <hovi6337@gmail.com>
2024-01-15 12:08:18 +0100

    Release-related scripts.

---
+++ release/stop.bat
@@ -0,0 +1,10 @@
+@echo off
+setlocal
+set URL=http://localhost:8002
+set HOME=%USERPROFILE%
+set CHARLIE_MNEMONIC_USER_DIR=%HOME%\AppData\Roaming\charlie-mnemonic\users
+
+echo Stopping Charlie Mnemonic using Docker Compose...
+docker-compose --project-name charlie-mnemonic-windows down
+
+endlocal

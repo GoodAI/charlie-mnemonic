7a1b862d0d37aed6bf6deb5d14b96fdc6a020efd
Karel Hovorka <hovi6337@gmail.com>
2024-01-25 20:15:34 +0100

    Fix
    Middleware fix.
    
    Middleware fix.

--- configuration_page/user_hack_middleware.py
+++ configuration_page/user_hack_middleware.py
@@ -2,18 +2,20 @@ from fastapi import Request
 from starlette.middleware.base import BaseHTTPMiddleware
 from starlette.responses import RedirectResponse
 
-from classes import LoginUser
-from routes import login
+from authentication import Authentication
+from routes import set_login_cookies
 
 
 class LoginAdminMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
         response = await call_next(request)
-        if request.method == "GET":
-            session_token = request.cookies.get("session_token")
+        session_token = request.cookies.get("session_token")
 
-            if session_token is None:
-                await login(LoginUser(username="admin", password="admin"), response)
-                return RedirectResponse(url=request.url.path)
+        if session_token is None:
+            auth = Authentication()
+            username = "admin"
+            password = "admin"
+            session_token = auth.login(username, password)
+            set_login_cookies(session_token, username, response)
 
         return response
